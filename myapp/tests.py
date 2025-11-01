85a38616feb33bd018cc39b3c1506a23478ad6a2
amanSin1 <amanboe1@gmail.com>
2025-11-01 11:32:59 +0530

    Initial Django project with Docker

---
+++ myapp/tests.py
@@ -0,0 +1,14 @@
+from django.test import TestCase, Client
+
+class SimpleTest(TestCase):
+    def test_hello_endpoint(self):
+        client = Client()
+        response = client.get('/hello/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()['status'], 'success')
+    
+    def test_health_endpoint(self):
+        client = Client()
+        response = client.get('/health/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()['health'], 'OK')
